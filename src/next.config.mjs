e92977c725a66b6faf1f3efc327ef0b9194c8423
tu.van <29675175+congtuuit@users.noreply.github.com>
2025-10-23 12:13:02 +0700

    config build

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -1,18 +1,14 @@
 export default {
   reactStrictMode: true,
-
   experimental: { appDir: true },
-
   output: 'export',
-
   images: {
     unoptimized: true,
   },
-
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-
   typescript: {
     ignoreBuildErrors: true,
   },
